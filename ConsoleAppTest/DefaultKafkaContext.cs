24708df9ab94ec6df70f77ba1d4bd66fa9c0681e
José Miguel <j.ramos@jm-ramos.com>
2025-07-05 14:42:19 +0200

    Add initial ConsoleAppTest project with Kafka context
    Introduces DefaultKafkaContext for handling 'orders.created' topic and a basic Program entry point. Sets up the foundation for Kafka-based message processing in the ConsoleAppTest project.

---
+++ ConsoleAppTest/DefaultKafkaContext.cs
@@ -0,0 +1,16 @@
+﻿using ConsoleAppTest.Models;
+using Net.Kafka.ReactiveOrm;
+using Net.Kafka.ReactiveOrm.Attributes;
+
+namespace ConsoleAppTest
+{
+    public class DefaultKafkaContext : KafkaContext
+    {
+        [Topic("orders.created", ConsumerGroup = "console-app")]
+        public TopicSet<OrderCreated> OrdersCreated { get; set; }
+
+        public DefaultKafkaContext(IKafkaBus bus) : base(bus)
+        {
+        }
+    }
+}
